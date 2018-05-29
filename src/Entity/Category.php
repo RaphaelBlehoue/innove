ce0d540a340c9a4e494c48938ce393d7edff848a
raphael Blehoue <raphaelsampan@gmail.com>
2018-05-29 14:20:19 +0000

    mise a jour de l'entity categories avec slug

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -6,6 +6,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints AS Assert;
+use Gedmo\Mapping\Annotation as Gedmo;
 
 
 /**
@@ -25,6 +26,12 @@ class Category
      * @Assert\NotBlank(message="Entrez le nom")
      */
     protected $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Gedmo\Slug(fields={"name", "id"}, separator="_", updatable=false)
+     */
+    protected $slug;
 
     /**
      * @ORM\Column(type="text", nullable=true)
@@ -174,6 +181,18 @@ class Category
                 $formation->setCategory(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
 
         return $this;
     }
